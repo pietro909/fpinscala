81fc488388a24edcf9faed0f9ae41ebf5c48dfe6
Pietro Grandi <pietro.grandi@evokegaming.com>
2017-02-15 21:41:30 +0100

    Correct final exercise

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -185,8 +185,7 @@ object State {
       _ <- set(f(s))
     } yield ()
 
-  type R = ((Int, Int), Machine)
-  private def getResult(m: Machine): R = ((m.coins, m.candies), m)
+  private def getResult(m: Machine): ((Int, Int), Machine) = ((m.coins, m.candies), m)
 
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
     State {
@@ -215,4 +214,21 @@ object State {
       }
     }
   }
+
+  def update = (i: Input) => (s: Machine) =>
+    (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    }
+
+  def simulateMachine2(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs map (modify[Machine] _ compose update))
+    s <- get
+  } yield (s.coins, s.candies)
+
 }
